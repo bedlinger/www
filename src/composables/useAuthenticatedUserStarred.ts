86e4b66fbca22c0fddbafb71ec90de6e12afba10
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-05-15 20:33:53 +0200

    fix typo in error state variable for starred items

--- src/composables/useAuthenticatedUserStarred.ts
+++ src/composables/useAuthenticatedUserStarred.ts
@@ -7,17 +7,17 @@ type Starred = Endpoints['GET /user/starred']['response']['data']
 export function useAuthenticatedUserStarred() {
   const starred = ref<Starred | null>(null)
   const isLoadingStarred = ref(false)
-  const errorStarreds = ref<Error | null>(null)
+  const errorStarred = ref<Error | null>(null)
 
   const fetchStarred = async () => {
     isLoadingStarred.value = true
-    errorStarreds.value = null
+    errorStarred.value = null
     try {
       const response = await octokit.request('GET /user/starred')
       starred.value = response.data
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
-      errorStarreds.value = err
+      errorStarred.value = err
     } finally {
       isLoadingStarred.value = false
     }
@@ -26,7 +26,7 @@ export function useAuthenticatedUserStarred() {
   return {
     starred,
     isLoadingStarred,
-    errorStarreds,
+    errorStarred,
     fetchStarred,
   }
 }
