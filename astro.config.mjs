a10dc8f79617705495f1386dc8bd1b444f9147c9
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-07-16 23:08:05 +0200

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,24 @@ import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	site: "https://bedlinger.at",
+
 	integrations: [
 		sitemap(),
 		icon({
 			iconDir: "src/assets/icons",
 		}),
 	],
+
+	adapter: node({
+		mode: "standalone",
+	}),
 });
