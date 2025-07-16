19fc0f8e83ae171349a4ec019513cfccbd8e94f4
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-07-16 23:29:22 +0200

    add octokit service

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+	readonly GITHUB_PAT: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
---
+++ src/services/octokitService.ts
@@ -0,0 +1,7 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+	auth: import.meta.env.GITHUB_PAT,
+});
+
+export default octokit;
